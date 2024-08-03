9f2dd62422e2f9405409f181cbda259e75349eb9
radicaled <daniel.bianco@outlook.com>
2024-08-03 16:41:00 -0300

    modules and packages

--- 06-Modules and Packages/00-Modules_and_Packages/myprogram.py
+++ 06-Modules and Packages/00-Modules_and_Packages/myprogram.py
@@ -1,6 +1,8 @@
 from MyMainPackage.some_main_script import report_main
 from MyMainPackage.SubPackage import mysubscript
+from mymodule import my_func
 
 report_main()
+my_func()
 
 mysubscript.sub_report()
